a078880575be0c307cc6e84c561627da613af9d1
roshanmthomas <42859882+roshanmthomas@users.noreply.github.com>
2023-05-27 13:56:43 +0530

    Update sample_audio.py

--- sample_audio.py
+++ sample_audio.py
@@ -1,3 +1,18 @@
 import pygame
 
+# Initialize Pygame
+pygame.init()
 
+# Load the alarm sound file
+alarm_sound_path = r"/audio/wake_up.wav"
+pygame.mixer.music.load(alarm_sound_path)
+
+# Play the alarm sound
+pygame.mixer.music.play()
+
+# Wait for the sound to finish playing
+while pygame.mixer.music.get_busy():
+    continue
+
+# Quit Pygame
+pygame.quit()
